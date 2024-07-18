796c7078251bc64f301630cc908ae90ac66ad984
Sigmanificient <edhyjox@gmail.com>
2024-07-18 23:08:00 +0200

    Setup arg parser

--- velma/__main__.py
+++ velma/__main__.py
@@ -1,10 +1,49 @@
 from . import __version__
 
-from typing import Final
+import argparse
+import sys
+
+from typing import Final, Any
 
 EXIT_SUCCESS: Final[int] = 0
 
+CLI_ARGS: dict[tuple[str, ...] | str, dict[str, Any]] = {
+    ("-p", "--profile"): dict(
+        required=True,
+        type=str,
+        help="execute all rules from the given profile",
+    ),
+    ("-r", "--root"): dict(
+        required=True,
+        type=str,
+        help="use the given directory as the vera root directory",
+    ),
+    "--version": dict(
+        action="version", version=f"{__package__}, v{__version__}"
+    ),
+    "files": dict(
+        nargs="*",
+        help="list of files. alternatively they may be sent via stdin. ",
+    ),
+}
+
 
 def main() -> int:
-    print(f"{__package__}, v{__version__}")
+    parser = argparse.ArgumentParser()
+
+    for flags, settings in CLI_ARGS.items():
+        if isinstance(flags, str):
+            flags = (flags,)
+
+        parser.add_argument(*flags, **settings)
+
+    args = parser.parse_args()
+
+    profile = args.profile
+    root = args.root
+    files = args.files or sys.stdin.read()
+
+    print(f"profile: {profile}, root: {root}")
+    print(files)
+
     return EXIT_SUCCESS
